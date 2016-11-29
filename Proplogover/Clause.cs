6fcbbfbc5d3c5edc4c312e62c28283a5de6f4374
Kai Weber <sermo_de_arboribus@seznam.cz>
2016-11-29 15:54:49 +0100

    Projektdateien hinzufügen.

---
+++ Proplogover/Clause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Proplogover
+{
+    /// <summary>
+    /// A clause is a disjunction of literals, as it can appear in a propositional logic formula in CNF
+    /// </summary>
+    public class Clause
+    {
+        #region Private fields
+
+        private HashSet<Literal> _clause;
+
+        #endregion
+
+        #region Constructor
+
+        public Clause(IEnumerable<Literal> clauseLiterals)
+        {
+            _clause.UnionWith(clauseLiterals);
+        }
+
+        #endregion
+
+        #region Public instance methods
+
+        public bool Evaluate()
+        {
+            IEnumerator<Literal> it = _clause.GetEnumerator();
+
+            bool result;
+            if(it.MoveNext())
+            {
+                // initial assignment to result variable
+                result = it.Current.Evaluate();
+                while(it.MoveNext())
+                {
+                    result |= it.Current.Evaluate();
+                }
+            }
+            else
+            {
+                // we are dealing with an empty clause
+                result = true;
+            }
+
+            return result;
+        }
+
+        #endregion
+    }
+}
---
+++ Proplogover/Literal.cs
@@ -0,0 +1,65 @@
+﻿namespace Proplogover
+{
+    /// <summary>
+    /// This class represents a boolean variable which appears as a literal in a formula of propositional logic.
+    /// Every literal has a name, a value and a sign (indicating if the value is to be modified by a NOT operator)
+    /// </summary>
+    public class Literal
+    {
+        #region Properties
+
+        public string Name { get; private set; }
+        /// <summary>
+        /// The Sign represents the NOT operator, which is treated as a property of the Literal.
+        /// Sign == false means that there is no NOT operator, Sign == true means a NOT operator is used
+        /// </summary>
+        public bool Sign { get; private set; }
+        public bool Value { get; set; }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor of a Literal
+        /// </summary>
+        /// <param name="name">The (immutable) name associated with this literal.</param>
+        /// <param name="sign">The (immutable) sign associated with this literal.</param>
+        public Literal(string name, bool sign)
+        {
+            Name = name;
+            Sign = sign;
+        }
+
+        #endregion
+
+        #region Public instance methods
+
+        public override bool Equals(object otherObject)
+        {
+            if(otherObject == null || GetType() != otherObject.GetType())
+            {
+                return false;
+            }
+
+            Literal otherLiteral = otherObject as Literal;
+            return this.Name == otherLiteral.Name && this.Sign == otherLiteral.Sign;
+        }
+
+        /// <summary>
+        /// Evaluation of this Literal.
+        /// </summary>
+        /// <returns>This method returns the current value of this Literal, considering the NOT operator / Sign.</returns>
+        public bool Evaluate()
+        {
+            return Sign ? !Value : Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return 13 * Name.GetHashCode() + 17 * Sign.GetHashCode();
+        }
+
+        #endregion
+    }
+}
